04211a3647636a0ec142a8dcfaf3373de31b220b
jasonbowman55 <95401132+jasonbowman55@users.noreply.github.com>
2024-10-21 23:13:51 -0700

    EXTI done, USART not done correctly

--- MCU/SEGGER_proj/main.c
+++ MCU/SEGGER_proj/main.c
@@ -19,15 +19,18 @@
 #define A_IN_PIN PA8 //"FT_a" 5V capatible
 #define B_IN_PIN PA6 //"FT_a" 5V capatible
 
-//void delayTIMinit() { //TIM2 = 1000Hz -> 1ms delay
-//  uint32_t PSCval = uint32_t (SystemCoreClock/4000); //divide 4MHz/4000 = 1000Hz
-//  TIM2->PSC &= ~(65535); //clear TIM2_PSC
-//  TIM2->PSC |= (PSCval); //enter calsulated PSC val to result in 1000Hz count
+//Global variables
+#define interupt_flag     //the internal software flag that says there was an interupt that happened
+unsigned char direction   //1 = clockwise and 0 = counter clockwise
+int state                 //this tells you the state which the interupts are at (if Ainterupt and Binterupt are on or off) [0,1,2,3] encoding
+int PulseCount            //pulses per revolution (1 pulse is when A and B interupts both equal 1
+int rps                   //number of S it took for 1 revolution
+int msPR                  //number of ms it took for 1 revolution
+int PPR = 360;            //NOTE: need to change likely through experimentation
+int A_on                  //internal flag saying that interupt A is triggered
+int B_on                  //internal flag saying that interupt B is triggered
 
-//  TIM2->EGR |= (TIM_EGR_UG); //generate update event to update PSC
-//  TIM2->CR1 |= (TIM_CR1_CEN); //enable TIM2 counter
-//}
-
+//********************************
 void GPIOinit() { //GPIO PA8 & PA6 enable
   gpioEnable(GPIO_PORT_A); //enable GPIOA
 
@@ -46,7 +49,7 @@ void SYSCLKinit() { //SYSCLK = MSI (4MHz)
   RCC->CR |= _FLD2VAL(RCC_CR_MSIRANGE, 0b0110); //set MSI as defalut 4MHz
 }
 
-void countTIMinit() { //TIM6 = SYSCLK = 4MHz
+void countTIMinit() { //TIM6
   RCC->APB1ENR1 |= RCC_APB1ENR1_TIM6EN;
   TIM6->PSC &= ~(65535); //clear TIM6_PSC
   uint32_t PSCval = uint32_t (SystemCoreClock/1000); //make counter 1ms
@@ -69,23 +72,145 @@ void EXTIcfgr() { //configure external interrupts specific for this project
     EXTI->FTSR1 |= _VAL2FLD(EXTI_FTSR1_FT8, 1); 
 }
 
+void delay(int ms) { // Function to create a delay using TIM6
+    for (int i = 0; i < ms; i++) {
+        TIM6->CNT = 0; // Reset the timer count
+        while (TIM6->CNT < 1); // Wait until the count reaches 1 (1 ms)
+    }
+}
 
+void rps_calc(int state) { //calculates rps based on the state
+    
+  //+1 to PulseCount every time A and B interupts are hi
+  if (state == 3) {
+    PulseCount = PulseCount + 1;
+  }
+  /////
 
-
+  //outputs how many seconds it took for one revolution
+  while (PulseCount < PPR) {
+    delay(1); //delay 1ms
+    msPR = msPR + 1;
+    rps = msPR / 1000;
+  }
+  /////
+}
+//********************************
 
 int main(void) {
- GPIOinit(); //initialize GPIOs
- SYSCLKinit(); //initialize 4MHz MSI SYSCLK
- countTIMinit(); //TIM6 init
+  GPIOinit(); //initialize GPIOs
+  SYSCLKinit(); //initialize 4MHz MSI SYSCLK
+  countTIMinit(); //TIM6 init
 
   __enable_irq(); // enable global interupts
   
   EXTIcfgr(); //configure external interrupts specific for this project
 
-  // not sure where the ISER comes from, but it relates to table 46 in the reference manual
-  // EXTI9_5_IQRn corresponds to the region of external interupts that our pins are on (PA6 and PA8)
-  NVIC->ISER[0] |= (1 << EXTI9_5_IRQn);
+  NVIC->ISER[0] |= (1 << EXTI9_5_IRQn); //turn on bitmask region relating to pins PA6 & PA8
   
+  rps_calc(int state); //calculated the rps and loop in here
+
+  void updateDirection(int state) {
+    if (A_on) {
+      switch(state) {
+          case 0:
+              delay(700);
+              printf("Direction: Clockwise\n");         // CW
+              printf("rps: %d\n", rps);
+              break;
+          case 1:
+              delay(700);
+              printf("Direction: Counter-Clockwise\n"); // CC
+              printf("rps: %d\n", rps);
+              break;
+          case 2:
+              delay(700);
+              printf("Direction: Counter-Clockwise\n"); // CC
+              printf("rps: %d\n", rps);
+              break;
+          case 3:
+              delay(700);
+              printf("Direction: Clockwise\n");         // CW
+              printf("rps: %d\n", rps);
+              break;
+          default:
+              delay(700);
+              printf("Unknown state\n");
+              break;
+      }
+      A_on = 0;
+    }
+
+    if (B_on) {
+      switch(state) {
+          case 0:
+              delay(700);
+              printf("Direction: Counter-Clockwise\n"); // CC
+              printf("rps: %d\n", rps);
+              break;
+          case 1:
+              delay(700);
+              printf("Direction: Clockwise\n");         // CW
+              printf("rps: %d\n", rps);
+              break;
+          case 2:
+              delay(700);
+              printf("Direction: Counter-Clockwise\n"); // CC
+              printf("rps: %d\n", rps);
+              break;
+          case 3:
+              delay(700);
+              printf("Direction: Clockwise\n");         // CW
+              printf("rps: %d\n", rps);
+              break;
+          default:
+              delay(700);
+              printf("Unknown state\n");
+              break;
+      }
+      B_on = 0;
+    }
+  }
+}
 
 
+void EXTI9_5_IRQHandler(void) {
+  int Ainterupt = digitalRead(A_IN_PIN); //reading the value of PA8 through the on board 5V ADC
+  int Binterupt = digitalRead(B_IN_PIN); //reading the value of PA6 through the on board 5V ADC
+  state = Ainterupt + (2 * Binterupt);   //creates a 0,1,2,3 encoding for the state of the interupts
+  //NOTE: 0=A_low B_low / 1=A_hi B_low / 2=A_low B_hi / 3=A_hi B_hi
+    
+  //NOTE: is the indedxing correct below? I remember someone saying that this part was strange
+
+    //interupt A (PA8) triggered the interupt
+    if (EXTI->PR1 & (1 << 8)){
+      A_on = 1; //internal flag signaling A interupt is triggered
+       switch(state){
+          case 0:             //0=A_low B_low
+            direction = 1;    //CW
+          case 1:             //1=A_hi B_low
+            direction = 0;    //CC
+          case 2:             //2=A_low B_hi
+            direction = 0;    //CC
+          case 3:             //3=A_hi B_hi
+            direction = 1;    //CW
+       }
+      EXTI->PR1 |= (1 << 8); //clear the interupt flag
+    }
+
+    //interupt B (PA6) triggered the interupt
+    if (EXTI->PR1 & (1 << 6)){ //if interupt A (PA8) triggered the interupt
+      B_on = 1; //internal flag signaling B interupt is triggered
+       switch(state){
+          case 0:             //0=A_low B_low
+            direction = 0;    //CC
+          case 1:             //1=A_hi B_low
+            direction = 1;    //CW
+          case 2:             //2=A_low B_hi
+            direction = 1;    //CC
+          case 3:             //3=A_hi B_hi
+            direction = 0;    //CW
+       }
+      EXTI->PR1 |= (1 << 6); //clear the interupt flag
+    }
 }
