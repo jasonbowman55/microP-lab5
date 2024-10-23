a8abea1e394b9571333a4f51f4ae1485755fbf5a
Jason <jbowman@hmc.edu>
2024-10-23 14:10:56 -0700

    working but math is just wrong

--- MCU/SEGGER_proj/Output/Debug/Obj/lab5_jb_segger/STM32L432KC_TIM.d
+++ MCU/SEGGER_proj/Output/Debug/Obj/lab5_jb_segger/STM32L432KC_TIM.d
@@ -1,17 +1,17 @@
 Output/Debug/Obj/lab5_jb_segger/STM32L432KC_TIM.o: \
- /Users/jasonbowman/Desktop/Micro-Ps/microP-lab5/MCU/SEGGER_proj/lib/STM32L432KC_TIM.c \
- /Users/jasonbowman/Desktop/Micro-Ps/microP-lab5/MCU/SEGGER_proj/lib/STM32L432KC_TIM.h \
- /Applications/SEGGER/SEGGER\ Embedded\ Studio\ 8.16a/include/stdint.h \
- /Applications/SEGGER/SEGGER\ Embedded\ Studio\ 8.16a/include/__SEGGER_RTL.h \
- /Applications/SEGGER/SEGGER\ Embedded\ Studio\ 8.16a/include/__SEGGER_RTL_ConfDefaults.h \
- /Applications/SEGGER/SEGGER\ Embedded\ Studio\ 8.16a/include/__SEGGER_RTL_Conf.h \
- /Applications/SEGGER/SEGGER\ Embedded\ Studio\ 8.16a/include/__SEGGER_RTL_Arm_Conf.h \
- /Users/jasonbowman/Desktop/Micro-Ps/microP-lab5/MCU/SEGGER_proj/STM32L4xx/Device/Include/stm32l432xx.h \
- /Users/jasonbowman/Desktop/Micro-Ps/microP-lab5/MCU/SEGGER_proj/CMSIS_5/CMSIS/Core/Include/core_cm4.h \
- /Users/jasonbowman/Desktop/Micro-Ps/microP-lab5/MCU/SEGGER_proj/CMSIS_5/CMSIS/Core/Include/cmsis_version.h \
- /Users/jasonbowman/Desktop/Micro-Ps/microP-lab5/MCU/SEGGER_proj/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h \
- /Users/jasonbowman/Desktop/Micro-Ps/microP-lab5/MCU/SEGGER_proj/CMSIS_5/CMSIS/Core/Include/cmsis_gcc.h \
- /Users/jasonbowman/Desktop/Micro-Ps/microP-lab5/MCU/SEGGER_proj/CMSIS_5/CMSIS/Core/Include/mpu_armv7.h \
- /Users/jasonbowman/Desktop/Micro-Ps/microP-lab5/MCU/SEGGER_proj/STM32L4xx/Device/Include/system_stm32l4xx.h \
- /Users/jasonbowman/Desktop/Micro-Ps/microP-lab5/MCU/SEGGER_proj/lib/STM32L432KC_GPIO.h \
- /Users/jasonbowman/Desktop/Micro-Ps/microP-lab5/MCU/SEGGER_proj/lib/STM32L432KC_RCC.h
+ C:\Users\jbowman\Desktop\microP-lab5\MCU\SEGGER_proj\lib\STM32L432KC_TIM.c \
+ C:\Users\jbowman\Desktop\microP-lab5\MCU\SEGGER_proj\lib\STM32L432KC_TIM.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ 8.16/include/stdint.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ 8.16/include/__SEGGER_RTL.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ 8.16/include/__SEGGER_RTL_ConfDefaults.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ 8.16/include/__SEGGER_RTL_Conf.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ 8.16/include/__SEGGER_RTL_Arm_Conf.h \
+ C:/Users/jbowman/Desktop/microP-lab5/MCU/SEGGER_proj/STM32L4xx/Device/Include/stm32l432xx.h \
+ C:/Users/jbowman/Desktop/microP-lab5/MCU/SEGGER_proj/CMSIS_5/CMSIS/Core/Include/core_cm4.h \
+ C:/Users/jbowman/Desktop/microP-lab5/MCU/SEGGER_proj/CMSIS_5/CMSIS/Core/Include/cmsis_version.h \
+ C:/Users/jbowman/Desktop/microP-lab5/MCU/SEGGER_proj/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h \
+ C:/Users/jbowman/Desktop/microP-lab5/MCU/SEGGER_proj/CMSIS_5/CMSIS/Core/Include/cmsis_gcc.h \
+ C:/Users/jbowman/Desktop/microP-lab5/MCU/SEGGER_proj/CMSIS_5/CMSIS/Core/Include/mpu_armv7.h \
+ C:/Users/jbowman/Desktop/microP-lab5/MCU/SEGGER_proj/STM32L4xx/Device/Include/system_stm32l4xx.h \
+ C:\Users\jbowman\Desktop\microP-lab5\MCU\SEGGER_proj\lib\STM32L432KC_GPIO.h \
+ C:\Users\jbowman\Desktop\microP-lab5\MCU\SEGGER_proj\lib\STM32L432KC_RCC.h
--- MCU/SEGGER_proj/main.c
+++ MCU/SEGGER_proj/main.c
@@ -11,7 +11,7 @@
 #include "STM32L432KC_TIM.h"
 #include "STM32L432KC_FLASH.h"
 #include "STM32L432KC_TIM.h"
-//#include <math.h>
+#include <math.h>
 
 
 // Necessary includes for printf to work
@@ -33,13 +33,16 @@ int _write(int file, char *ptr, int len) {
 
 //Global variables
 #define interupt_flag      //the internal software flag that says there was an interupt that happened
-#define COUNT_TIM TIM2     //make TIM2 to be the counter timer
-#define DELAY_TIM TIM6     //make TIM6 to be the delay timer
+#define COUNT_TIM TIM6     //make TIM2 to be the counter timer
+#define DELAY_TIM TIM2    //make TIM6 to be the delay timer
 int direction;             //1 = clockwise and 0 = counter clockwise
-int delta = 0;                 //the number of 
+unsigned int delta;                 //the number of 
 int rpm = 0;                   //number of S it took for 1 revolution
 //int PPR = 100;             //NOTE: need to find it in the data sheet
 int still = 1;                 //1=not moving motor, 0=moving motor
+int A;
+int B;
+
 
 //********************************
 void GPIOinit() {                   //GPIO PA8 & PA6 enable
@@ -67,12 +70,12 @@ void EXTIcfgr() {                   //configure external interrupts specific for
     EXTI->FTSR1 |= _VAL2FLD(EXTI_FTSR1_FT8, 1); 
 }
 
-void delay(int ms) {                //Function to create a delay using TIM6
-    for (int i = 0; i < ms; i++) {
-        DELAY_TIM->CNT = 0;              //Reset the timer count
-        while (DELAY_TIM->CNT < 1);      //Wait until the count reaches 1 (1 ms)
-    }
-}
+//void delay(int ms) {                //Function to create a delay using TIM6
+//    for (int i = 0; i < ms; i++) {
+//        DELAY_TIM->CNT = 0;              //Reset the timer count
+//        while (DELAY_TIM->CNT < 1);      //Wait until the count reaches 1 (1 ms)
+//    }
+//}
 
 //void rpm_calc (delta) {         //calculate the RPS and Direction of the motor
 //  int ms_per_rev = fabs(delta) * PPR;   //time per revolution in ms
@@ -127,7 +130,7 @@ int main(void) {
         // else calculations for motor speed
         }else {
           if(rpm == 0){
-            rpm1 = 1/(double)(120*delta*4/1000000.0);
+            rpm1 = 1/(double)(120*abs(delta)*4/1000000.0);
             rpm2 = rpm1;
             rpm3 = rpm1;
             rpm4 = rpm1;
@@ -135,13 +138,14 @@ int main(void) {
             rpm1 = rpm2;
             rpm2 = rpm3;
             rpm3 = rpm4;
-            rpm4 = 1/(double)(120*delta*4/1000000.0);
+            rpm4 = 1/(double)(120*abs(delta)*4/1000000.0);
           }
           rpm = (rpm1+rpm2+rpm3+rpm4)/4;
         }
         
         printf("Revolutions per Second: %f\n", rpm);
         printf("Delta: %d\n", delta);
+
     }
 }
 
@@ -150,28 +154,35 @@ int main(void) {
 void EXTI9_5_IRQHandler(void) { //outputs delta (the time between A=1 and B=1 interupts
   int Ainterupt = digitalRead(A_IN_PIN); //reading the value of PA8 through the on board 5V ADC
   int Binterupt = digitalRead(B_IN_PIN); //reading the value of PA6 through the on board 5V ADC
+  //unsigned int initial_tim = COUNT_TIM->CNT;
+  A = Ainterupt;
+  B = COUNT_TIM->CNT;
 
   //if A interupt happens
   if (EXTI->PR1 & (1 << 8)){
-    EXTI->PR1 |= (1 << 8); //clear the interupt flag
-    COUNT_TIM->CNT = 0;    //set the counter to be 0
+    //EXTI->PR1 |= (1 << 8); //clear the interupt flag
     still = 0;             //the motor is not still
+    //printf("Delta1: %d\n", A);
+    //printf("Delta1: %d\n", B);
 
     if((Binterupt==1) && (Ainterupt==1)){ //if a pulse occurs
       delta = COUNT_TIM->CNT; //clock cycles going CW
+       printf("Delta1: %d\n", delta);
     }
+    EXTI->PR1 |= (1 << 8); //clear the interupt flag
     COUNT_TIM->CNT = 0; //reset counter
   }
 
   //if B interupt happens
   if (EXTI->PR1 & (1 << 6)){
-    EXTI->PR1 |= (1 << 6); //clear the interupt flag
-    //COUNT_TIM->CNT = 0;    //set the counter to be 0
+    //EXTI->PR1 |= (1 << 6); //clear the interupt flag
     still = 0;             //the motor is not still
 
    if((Binterupt==1) && (Ainterupt==1)){ //if a pulse occurs
       delta = -COUNT_TIM->CNT; //clock cycles going CCW
+      printf("Delta2: %d\n", delta);
     }
+    EXTI->PR1 |= (1 << 6); //clear the interupt flag
     COUNT_TIM->CNT = 0; //reset counter
   }
 }
